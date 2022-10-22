3810d9437e1bfc52dc04e8c993bc562103b1f567
robert kaczur <kaczurr@gmail.com>
2022-10-22 11:16:15 -0400

    finish user routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require('../models/user.js');
+const userModel = require('../model/user.js');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res){
   res.send('respond with a resource');
 });
 
@@ -24,61 +24,20 @@ router.post('/signup', async (req, res) => {
   }
 });
 
-router.get('/notes', async (req, res) => {
-  const notes = await noteModel.find({});    
-  try{
-      res.send(notes);
-  }catch(err){
-      res.status(500).send(err);
-  }
-
-});
-
-router.get('/notes/:noteId', async (req, res) => {
-  // Validate request
-  if(!req.params.noteId) {
-      return res.status(400).send({
-          message: "Note content can not be empty"
-      });
-  }
-
-  const note = await noteModel.findById(req.params.noteId); 
-  try{
-      res.send(note);
-  }catch(err){
-      res.status(500).send(err);
-  }
-
-});
-
-//TODO - Update a Note with noteId
-router.put('/notes/:noteId', async(req, res) => {
-  console.log(req.params.noteId)
-  console.log(req.body)
-  try{
-      const updatedNote = await noteModel.findByIdAndUpdate(req.params.noteId, req.body)
-      const note = await updatedNote.save()
-      res.send(note)
-  }catch (err){
-      console.log(err)
-      res.status(500).send(err);
-  }
-
-});
-
-router.delete('/notes/:noteId', async (req, res) => {
-  // Validate request
-  try {
-      const note = await noteModel.findByIdAndDelete(req.params.noteId)
-  
-      if (!note) { 
-          res.status(404).send("No item found")
-      }
-      res.status(200).send(note)
-    } catch (err) {
-      res.status(500).send(err)
-    }
-});
+//login
+router.post('/login',
+    async (req, res) => {
+        try {
+            const user = await userModel.username(req.body.username);
+            if (user.password === req.body.password) {
+                res.status(200).send("logged in")
+            } else {
+                res.status(400).send("invalid login")
+            }
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    });
 
 
 module.exports = router;
